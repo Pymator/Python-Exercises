eadfcf6c3c55ff7280ab4f017ee8c56a1405058f
LDziedzic <pymator@gmail.com>
2020-01-03 12:11:02 +0100

    Add 6.py file

---
+++ basic-part-1/6.py
@@ -0,0 +1,7 @@
+numbers = input('Enter the sequence of comma-separated numbers: ')
+
+numbers_list = numbers.split(',')
+numbers_tuple = tuple(numbers_list)
+
+print(numbers_list)
+print(numbers_tuple)
