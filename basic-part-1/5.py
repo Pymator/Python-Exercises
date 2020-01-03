13fd7f0aca9c4f07b1e2bc207708f67ebde0d997
LDziedzic <pymator@gmail.com>
2020-01-03 12:06:19 +0100

    Add 5.py file

---
+++ basic-part-1/5.py
@@ -0,0 +1,21 @@
+# Method 1
+first_name = ''
+last_name = ''
+
+while first_name == '':
+    first_name = input('Enter your first_name: ')
+
+while last_name == '':
+    last_name = input('Enter yout last_name: ')
+
+print(f'Hi {last_name} {first_name}')
+
+# Method 2
+# name = ''
+
+# while name == '':
+#     name = input('Enter your first name and last name: ')
+
+# names = name.split()
+
+# print(f'Hi {names[-1]} {names[-2]}')
